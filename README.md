8d63a40ebdb45bda3e85094f4d7da4c5a89f2b4b
sweetroll <sweetrollbandit@protonmail.com>
2022-09-10 10:17:44 -0500

    renamed to frontend

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ctf.paris.systems
+
+This is a capture-the-flag site created by SugarSecurity.com
+
+## Rules
+
+1. No DoS / DDoS
+2. Please limit dirbusting and other HTTP fuzzing. We pay-per-request and the site goes down if costs exceed $100/month
+3. Anything under "ctf.paris.systems" is fair game. Please do not target other paris.systems subdomains
---
+++ frontend/pages/index.tsx
@@ -0,0 +1,53 @@
+import type { NextPage } from 'next'
+import { useRouter } from "next/router"
+
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+
+  const router = useRouter();
+  const { locale } = router;
+
+  const switchLanguage = (e) => {
+    const locale = e.target.value;
+    router.push('/','/', { locale });
+  }
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Paris CTF</title>
+        <meta name="description" content="by sugarsecurity.com" />
+        <link rel="icon" href="/tower.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          <span className={styles.logo}>
+            <Image src="/tower.png" alt="Eiffel Tower" width={60} height={100}/>
+          </span>
+                  Welcome to Paris!
+        </h1>
+
+        <p className={styles.description}>
+          select language:
+          <select
+            onChange={switchLanguage}
+            defaultValue={locale}
+          >
+            <option value="en">EN</option>
+            <option value="es">ES</option>
+          </select>
+        </p>
+      </main>
+
+      <footer className={styles.footer}>
+        
+      </footer>
+    </div>
+  )
+}
+
+export default Home
